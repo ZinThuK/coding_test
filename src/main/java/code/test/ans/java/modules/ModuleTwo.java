81963e8c7e16a991246d5f1a188875a04fc4978a
zinthukhaing <zinthukhaing@localhost>
2025-03-08 12:51:34 +0900

    Correct comment

--- src/main/java/code/test/ans/java/modules/ModuleTwo.java
+++ src/main/java/code/test/ans/java/modules/ModuleTwo.java
@@ -12,8 +12,10 @@ public class ModuleTwo {
 	    .toArray();
 
 		// k番目に大きい要素はインデックス k-1 にある（0始まりのため）
-		System.out.println("Index Number: " + (nums.length - k));
-		System.out.println("Value: " + nums[nums.length - k]);
+		System.out.println("k = " +k);
+		System.out.println("After sorting nums: " + Arrays.toString(nums));
+		System.out.println("Index Number: " + (k - 1));
+		System.out.println("Value: " + nums[k - 1]);
 
 		return nums[k - 1];
 	}
